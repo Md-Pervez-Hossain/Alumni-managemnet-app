04cb7d4ea2c144de4e35d426ab90083c7bd191b4
himelhasan <himelhasan497@gmail.com>
2023-03-30 22:40:50 +0600

    project setup done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
