6d29aa30380864cd5d299dc8d6ac58f59febd2f2
rakib38324 <rakib3832@gmail.com>
2023-03-31 01:58:34 +0600

    Complete Gallery Section and also responsive with mobile and tab divices

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import FundsForCause from "./Components/FundsForCause/FundsForCause";
+import Gallery from "./Components/Gallery/Gallery";
 
 
 
 function App() {
   return (
     <div className="w-9/12 mx-auto">
-      
+     
     </div>
   );
 }
