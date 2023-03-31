90658aa801add9cfd1ddb0adacd3390c0583775f
himelhasan <himelhasan497@gmail.com>
2023-03-31 11:43:40 +0600

    conflict issue of rakib solved

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
+import FundsForCause from "./Components/FundsForCause/FundsForCause";
+import Gallery from "./Components/Gallery/Gallery";
 
 function App() {
-  return <div></div>;
+  return <div className="w-9/12 mx-auto"></div>;
 }
 
 export default App;
