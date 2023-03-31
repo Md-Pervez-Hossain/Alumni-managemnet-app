a526f53631033f9144bd3c8118068393fee16f21
himelhasan <himelhasan497@gmail.com>
2023-03-31 11:34:07 +0600

    conflict code removed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from "react";
 import "./App.css";
-import SuccessStories from "./sharedComponents/SuccessStories/SuccessStories";
-import UpComingEvents from "./sharedComponents/UpComingEvents/UpComingEvents";
 
 function App() {
-  return (
-    <div>
-      <UpComingEvents />
-      <SuccessStories/>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
