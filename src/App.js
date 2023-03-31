fd3b3e251bd441b517452a8d23dec68184c03c7c
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2023-03-31 11:34:16 +0600

    add navber

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
 import "./App.css";
-import Header from "./Components/Header/Header";
-import Slider from "./Components/Slider/Slider";
-import MembersForm from "./Components/MembersForm/MembersForm";
-import Footer from "./Components/Footer/Footer";
 
 function App() {
   return <div></div>;
