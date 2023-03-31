a3796f80cf9bc1493e046df5a311542784de2f31
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2023-03-31 16:50:35 +0600

    member state update

--- src/sharedComponents/Header/Header.js
+++ src/sharedComponents/Header/Header.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { BiMenu } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
     <nav className="bg-green-500 text-white ">
-      <div className="w-9/12 mx-auto flex flex-wrap  list-none justify-between items-center py-5 text-xl font-bold">
+      <div className="w-9/12 mx-auto flex flex-wrap  list-none justify-between items-center py-5 text-xl font-semibold">
         <div>
           <li>Alumni</li>
         </div>
-        <div className="flex flex-wrap flex-col md:flex-row gap-5 ">
-          <li>Home</li>
-          <li>Events</li>
-          <li>News</li>
-          <li>Gallery</li>
+        <div onClick={() => setOpen(!open)}>
+          {open ? (
+            <>
+              {" "}
+              <AiOutlineClose className="md:hidden"></AiOutlineClose>{" "}
+            </>
+          ) : (
+            <>
+              {" "}
+              <BiMenu className="md:hidden"></BiMenu>
+            </>
+          )}
+          <div
+            className={`flex w-full left-0 px-4 py-3  bg-green-500 text-white md:ml-3  flex-col absolute md:static duration-700 ease-in-out md:flex-row gap-4 ${
+              open ? "top-16" : "top-[-200px]"
+            }`}
+          >
+            <li>Home</li>
+            <li>Events</li>
+            <li>News</li>
+            <li>Gallery</li>
+          </div>
         </div>
       </div>
     </nav>
--- src/sharedComponents/MembersStat/MembersStat.js
+++ src/sharedComponents/MembersStat/MembersStat.js
@@ -3,20 +3,20 @@ import React from "react";
 const MembersStat = () => {
   return (
     <div className="md:w-9/12 mx-auto my-16">
-      <div className="bg-gray-100 flex justify-around items-center py-16">
-        <div className="text-center">
+      <div className="bg-gray-100  grid  md:grid-cols-4 grid-cols-2 py-16">
+        <div className="text-center md:mb-0 mb-4">
           <h2 className="text-4xl font-semibold">Members</h2>
           <p className="text-2xl">3458+</p>
         </div>
-        <div className="text-center">
+        <div className="text-center md:mb-0 mb-4">
           <h2 className="text-4xl font-semibold">Events</h2>
           <p className="text-2xl">58+</p>
         </div>
-        <div className="text-center">
+        <div className="text-center md:mb-0 mb-4">
           <h2 className="text-4xl font-semibold">Awards</h2>
           <p className="text-2xl">3245+</p>
         </div>
-        <div className="text-center">
+        <div className="text-center md:mb-0 mb-4">
           <h2 className="text-4xl font-semibold">Picnic</h2>
           <p className="text-2xl">45+</p>
         </div>
