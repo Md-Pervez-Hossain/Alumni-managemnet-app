bf52301a7503aa6ec3b01330139bfdc06478ff73
nakibuddin <nakibaust137@gmail.com>
2023-03-31 16:10:09 +0600

    app file edited

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from "react";
 import "./App.css";
-import MissionHeading from "./sharedComponents/MissionHeading/MissionHeading";
-import AlumniDirectory from "./sharedComponents/AlumniDirectory/AlumniDirectory";
-import Contact from "./sharedComponents/Contact/Contact";
 
 function App() {
     return (
-        <div>
-            <Contact></Contact>
-            <MissionHeading></MissionHeading>
-            <AlumniDirectory></AlumniDirectory>            
+        <div>                        
         </div>
     );
 }
