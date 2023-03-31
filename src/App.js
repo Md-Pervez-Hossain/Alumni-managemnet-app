cb7fa8be58f8689f30a8d1b42a8ef4b0b9f5865c
nakibuddin <nakibaust137@gmail.com>
2023-03-31 15:49:27 +0600

    MissionHeading, AlumniDirectory and contact component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
+import MissionHeading from "./sharedComponents/MissionHeading/MissionHeading";
+import AlumniDirectory from "./sharedComponents/AlumniDirectory/AlumniDirectory";
+import Contact from "./sharedComponents/Contact/Contact";
 
 function App() {
-  return <div></div>;
+    return (
+        <div>
+            <Contact></Contact>
+            <MissionHeading></MissionHeading>
+            <AlumniDirectory></AlumniDirectory>            
+        </div>
+    );
 }
 
 export default App;
