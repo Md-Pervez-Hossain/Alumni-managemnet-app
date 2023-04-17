2795f241683529dfcd69d1997bce1662d7c8d60b
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2023-04-18 01:03:08 +0600

    single news create

--- src/Pages/TestingPage/Rakib.js
+++ src/Pages/TestingPage/Rakib.js
@@ -5,6 +5,7 @@ import Counter from "../../sharedComponents/Counter/Counter";
 import AlumniDashboard from "../../sharedComponents/AlumniDashboard/AlumniDashboard";
 import NewsCreateForm from "../../Dashboard/DashboardComponents/NewsCreateForm/NewsCreateForm";
 import CreateEvents from "../../Dashboard/DashboardComponents/CreateEvents/CreateEvents";
+import SingleNewsSection from "../../sharedComponents/NewsCards/SingleNewsSection";
 
 const Rakib = () => {
   return (
@@ -15,6 +16,7 @@ const Rakib = () => {
       <AlumniDashboard></AlumniDashboard>
       <NewsCreateForm />
       <CreateEvents></CreateEvents>
+      <SingleNewsSection></SingleNewsSection>
     </div>
   );
 };
