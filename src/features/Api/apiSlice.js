13e862a06dbb5ac98a9dc3cf97d7c44ff0ee0e3d
himelhasan <himelhasan497@gmail.com>
2023-04-18 01:36:52 +0600

    event date form updated

--- src/features/Api/apiSlice.js
+++ src/features/Api/apiSlice.js
@@ -10,7 +10,7 @@ export const apiSlice = createApi({
     getGalleries: builder.query({
       query: () => "/galleries ",
     }),
-    getCategoryWiseGallery20: builder.query({
+    getCategoryWiseGallery: builder.query({
       query: (id) => `/galleryCategories/${id}`,
     }),
     // /galleryCategories/:id GET endpoint that returns a single gallery category data based on the id parameter
@@ -32,11 +32,12 @@ export const apiSlice = createApi({
     getEventsCategories: builder.query({
       query: () => "/eventCategories",
     }),
-    getEventCategories: builder.query({
-      query: () => "/eventCategories",
-    }),
 
     // // news
+    getaLLNews: builder.query({
+      query: () => "/news",
+    }),
+
     getNewsCategories: builder.query({
       query: () => "/alumniNewsCategories",
     }),
@@ -57,14 +58,20 @@ export const apiSlice = createApi({
 });
 
 export const {
+  // NEWS
   useGetNewsCategoriesQuery,
+
+  // UTILS
   useGetAllBatchesQuery,
   useGetAllUniversityNameQuery,
-  useGetEventsCategoriesQuery,
+
+  // GALLERY
   useGetGalleriesQuery,
   useGetGalleryCategoriesQuery,
   useGetGalleriesFeaturedQuery,
   useGetGalleriesTrendingQuery,
+
+  // EVENTS
   useGetEventsQuery,
-  useGetEventCategoriesQuery,
+  useGetEventsCategoriesQuery,
 } = apiSlice;
