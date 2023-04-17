5de8091012edc1476a6e28d096cc454d2cf58e35
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2023-04-18 01:49:36 +0600

    single nes data

--- src/sharedComponents/NewsCards/MoreNews.js
+++ src/sharedComponents/NewsCards/MoreNews.js
@@ -2,21 +2,74 @@ import React from "react";
 
 const MoreNews = () => {
   return (
-    <div className="flex items-center gap-2 justify-between">
-      <div>
-        <img
-          src="https://images.pexels.com/photos/7944058/pexels-photo-7944058.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
-          className="h-36 w-80"
-        />
+    <div>
+      <div className="flex items-center gap-3 justify-between mb-5">
+        <div>
+          <img
+            src="https://images.pexels.com/photos/7944058/pexels-photo-7944058.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className="h-36 w-80"
+          />
+        </div>
+        <div>
+          <h2 className="mb-2">News Heading</h2>
+          <p className="text-[12px] mb-2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Dignissimos, in.
+          </p>
+          <button className="bg-secondary px-4 py-2 text-white">Details</button>
+        </div>
       </div>
-      <div>
-        <h2>News Heading</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos,
-          in.
-        </p>
-        <button className="bg-secondary px-6 py-2 text-white">Details</button>
+      <div className="flex items-center gap-3 justify-between mb-5">
+        <div>
+          <img
+            src="https://images.pexels.com/photos/7944058/pexels-photo-7944058.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className="h-36 w-80"
+          />
+        </div>
+        <div>
+          <h2 className="mb-2">News Heading</h2>
+          <p className="text-[12px] mb-2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Dignissimos, in.
+          </p>
+          <button className="bg-secondary px-4 py-2 text-white">Details</button>
+        </div>
+      </div>
+      <div className="flex items-center gap-3 justify-between mb-5">
+        <div>
+          <img
+            src="https://images.pexels.com/photos/7944058/pexels-photo-7944058.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className="h-36 w-80"
+          />
+        </div>
+        <div>
+          <h2 className="mb-2">News Heading</h2>
+          <p className="text-[12px] mb-2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Dignissimos, in.
+          </p>
+          <button className="bg-secondary px-4 py-2 text-white">Details</button>
+        </div>
+      </div>
+      <div className="flex items-center gap-3 justify-between mb-5">
+        <div>
+          <img
+            src="https://images.pexels.com/photos/7944058/pexels-photo-7944058.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className="h-36 w-80"
+          />
+        </div>
+        <div>
+          <h2 className="mb-2">News Heading</h2>
+          <p className="text-[12px] mb-2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Dignissimos, in.
+          </p>
+          <button className="bg-secondary px-4 py-2 text-white">Details</button>
+        </div>
       </div>
     </div>
   );
